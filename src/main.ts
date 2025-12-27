637879c4a830e6c999e5e69747a20de9d4dede0b
Luan Alves <luan@secid.pr.gov.br>
2025-12-27 17:37:41 -0300

    change api cors settings

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,14 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('veraomaior-api', app, documentFactory);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://verao-maior-app.vercel.app', 'https://veraomaior-secid.web.app/'],
-    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: [
+      'http://localhost:4200',
+      'https://verao-maior-app.vercel.app',
+      'https://veraomaior-secid.web.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(port);
 }
